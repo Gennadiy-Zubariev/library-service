a4de9a5ecf6aa9aa474002beb3e62588bb0c931f
Gennadiy-Zubariev <genada89@gmail.com>
2026-09-25 12:47:57 +0300

    feat(borrowings): add filtering by user_id for admin and by is_active for authenticated users

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from borrowings.views import BorrowingView
+from borrowings.views import BorrowingViewSet
 
 app_name = "borrowings"
 
 router = DefaultRouter()
-router.register("", BorrowingView, basename="borrowing")
+router.register("", BorrowingViewSet, basename="borrowing")
 
 urlpatterns = [
     path("", include(router.urls)),
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,15 +1,28 @@
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from books.models import Book
+from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingReadSerializer
 
 
-class BorrowingView(
+class BorrowingViewSet(
     mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
 ):
     serializer_class = BorrowingReadSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return Book.objects.filter(user=self.request.user)
+        queryset = Borrowing.objects.select_related("book")
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active and is_active.lower() == "true":
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        return queryset
